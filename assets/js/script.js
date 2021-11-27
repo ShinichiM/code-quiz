c5be6e536b162f1e6cc5985b22cdeedc7a2791a1
Shinichi <miyakawashi@gmail.com>
2021-11-26 23:55:16 -0500

    added form handling for high score intial submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,9 +3,16 @@ var startQuizButton = document.getElementById("start-quiz");
 var questionContainer = document.getElementById("question-container");
 var questionEl = document.getElementById("question");
 var currentQuestion;
+var numPoints = 0;
 
 var answerResponse = document.createElement("div");
-questionContainer.appendChild(answerResponse);
+answerResponse.setAttribute("id", "user-response");
+var buttonContainer = document.createElement("div");
+
+buttonContainer.setAttribute("class", "container");
+questionContainer.appendChild(buttonContainer);
+
+var users = []
 var timer = 60;
 
 var quizQuestions = [
@@ -29,13 +36,20 @@ var quizQuestions = [
         {text: "insert", correct: false},
         {text: "looper", correct: false},
         {text: "within", correct: false}
+    ]},
+    {question: "What is a common looping method in Javascript?",
+    answers: [
+        {text: "safdkjelwahfdjs;alkdsajkfl;eawfdsjkr", correct: true},
+        {text: "insert", correct: false},
+        {text: "looper", correct: false},
+        {text: "within", correct: false}
     ]}
 ];
 
 
 var countdown = function() {
     var divEl = document.getElementById("timer");
-    if (timer >= 0) {
+    if (timer >= 0){
         divEl.innerText = "Time: " + timer; 
         timer--;
     } else {
@@ -64,10 +78,85 @@ var clearQuestion = function() {
     }
 };
 
+// display Highscores
+var highscore = function(quizFinishDiv, finishText, userInitials) {
+    // localStorage.setItem(userInitials, numPoints);
+    questionEl.innerText = "High Scores";
+    finishText.remove();
+    answerResponse.remove();
+    quizFinishDiv.remove();
+};
+
+// display quiz finish
+var finishQuiz = function() {
+    var quizFinishDiv = document.createElement("div");
+    var quizFinishText = document.createElement("p");
+    var quizFinishInput = document.createElement("input");
+    var quizFinishLabel = document.createElement("label");
+    var quizFinishSubmit = document.createElement("input");
+    var highscoreForm = document.createElement("form");
+
+
+    questionEl.innerText = "All done!"
+
+    highscoreForm.setAttribute("id", "quiz-form");
+    quizFinishDiv.setAttribute("class", "left-flex");
+
+    quizFinishInput.setAttribute("type", "text");
+    quizFinishInput.setAttribute("id", "high-score");
+    quizFinishInput.setAttribute("class", "m-right");
+
+    quizFinishSubmit.setAttribute("type", "submit");
+    quizFinishSubmit.setAttribute("class", "submit-button");
+    quizFinishSubmit.setAttribute("id", "highscore");
+    
+    quizFinishLabel.setAttribute("for", "high-score");
+    quizFinishLabel.setAttribute("class", "m-right");
+
+    quizFinishText.setAttribute("class", "text-left");
+    quizFinishText.innerText = "Your final score is 22";
+    quizFinishLabel.innerText = "Enter Initials: ";
+
+
+    questionContainer.appendChild(quizFinishText);
+    quizFinishDiv.appendChild(quizFinishLabel);
+    quizFinishDiv.appendChild(quizFinishInput);
+    quizFinishDiv.appendChild(quizFinishSubmit);
+
+    highscoreForm.appendChild(quizFinishDiv);
+    
+    questionContainer.appendChild(highscoreForm);
+    clearQuestion();
+
+    // STORE SCORE IN LOCAL STORAGE
+    var test = document.querySelector("input[id='high-score'").value;
+    
+    var highscoreForm = document.getElementById("highscore");
+
+    highscoreForm.addEventListener("click", function(event){
+        event.preventDefault();
+        userInitals = quizFinishInput.value;
+
+        var userObj = {
+            initials: userInitals,
+            score: timer
+        };
+        users.push(userObj);
+
+        // localStorage.setItem("Points")
+        console.log(userInitals + " is it working?");
+        highscore(quizFinishDiv, quizFinishText, quizFinishInput.value);
+    }); 
+}
 
 // listen for user interaction with an answer button: when they (select an answer)
 var selectAnswer = function(event) {
-    answerResponse.setAttribute("class", "right-answer");
+
+    if (currentQuestion === 0) {
+        answerResponse.setAttribute("class", "response");
+        questionContainer.appendChild(answerResponse);
+    }
+
     // store element that user clicks
     var userChoice = event.target;
 
@@ -77,33 +166,41 @@ var selectAnswer = function(event) {
     // If correct, alert user correct. If not, remove 10 sec from timer and alert user wrong
     if (isCorrect === "true") {
         answerResponse.innerText = "Correct!";
+        numPoints += 10;
     } else {
         timer = timer - 10;
-        answerResponse.innerText = "Wrong!";
+        answerResponse.innerText = "Wrong!";    
     }
-    
-    // Increment question counter to select next question object.
-    currentQuestion++;
 
-    // clear current question from screen.
-    clearQuestion();
 
-    // display the next question
-    displayNextQuestion();
+    if (currentQuestion + 1 == quizQuestions.length) {
+        // call highscore function
+        finishQuiz();
+    } else {
+        // Increment question counter to select next question object.
+        currentQuestion++;
+
+        // clear current question from screen.
+        clearQuestion();
+
+        // display the next question
+        displayNextQuestion();
+    }
 };
 
 var displayQuestion = function(questionObj) {
+    questionEl.setAttribute("class", "container");
     questionEl.innerText = questionObj.question;
 
     // Loop through each answer in the question and create a button whose text value is the answer
     for (var i=0; i<questionObj.answers.length; i++) {
         // create a button for each answer in a question.
-        var button = document.createElement("button");
+        var button = document.createElement("button"); 
         button.setAttribute("id", "button");
         button.setAttribute("class", "answers");
-        button.innerText = questionObj.answers[i].text;
-        button.setAttribute("data-correct", questionObj.answers[i].correct)
-        questionContainer.appendChild(button); 
+        button.innerText = (i+1) + ". " + questionObj.answers[i].text;
+        button.setAttribute("data-correct", questionObj.answers[i].correct);
+        buttonContainer.appendChild(button); 
 
         button.addEventListener("click", selectAnswer);
     }
@@ -124,3 +221,4 @@ var startQuiz = function() {
 };
 
 startQuizButton.addEventListener("click", startQuiz);
+
